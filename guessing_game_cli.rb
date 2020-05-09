72a8213880e5c1cceba32861500d2c45bc7128a5
Logan McGuire <lrmcguire93@gmail.com>
2020-05-09 03:05:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -14,7 +14,7 @@ def get_user_input
   criteria.map(&:to_s);
   criteria << ['exit'];
   puts criteria; 
-  until criteria.include? user_input do 
+  until criteria.fetch(user_input, nil) != nil do 
     puts "Sorry, #{user_input} is not valid input."
     promt_user_input;
     user_input = get_user_input; 
