8f79dbd37a5d64b1edc15427ea70b78d05bd137f
Logan McGuire <lrmcguire93@gmail.com>
2020-05-09 02:52:23 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,50 @@
 # Code your solution here!
+
+def welcome 
+  puts "Hello, thanks for coming. Now let's play!"
+end 
+
+def generate_number
+  rand(1..6).to_s; 
+end
+
+def get_user_input
+  user_input = gets.chomp;
+  criteria = [1..6].to_s;
+  puts criteria;
+  criteria << 'exit';
+  puts criteria; 
+  until criteria.include? (user_input) do 
+    puts "Sorry, #{user_input} is not valid input."
+    promt_user_input;
+    user_input = get_user_input; 
+  end
+  user_input; 
+end
+
+def promt_user_input
+  puts "Please guess the computer's number! It could be 1-6, inclusive. Or use 'exit' to leave."
+end
+
+def using_input (user_input, comp_num)
+  case 
+    when user_input === comp_num
+      puts "You guessed the correct number!"
+    when user_input === 'exit'
+      puts "Goodbye!"; 
+    else 
+      puts "Sorry the computer guessed #{comp_num}."
+  end
+end
+
+def run_guessing_game 
+  welcome; 
+  user_input = '';
+  until user_input === 'exit' do 
+    comp_num = generate_number;
+    promt_user_input;
+    user_input = get_user_input;
+    using_input(user_input, comp_num);
+  end
+end
+
